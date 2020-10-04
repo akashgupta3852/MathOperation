a3a20cab83f91fa41b4a262fa533baa3bf7f861e
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-04 23:21:42 +0530

    [Akash]Added: lambda function as argument

--- src/com/mathoperation/MathApp.java
+++ src/com/mathoperation/MathApp.java
@@ -3,6 +3,9 @@ package com.mathoperation;
 @FunctionalInterface
 interface ICalculator{
 	public int calculate(int a, int b);
+	static void printResult(int x, int y, String method, ICalculator iCalculator) {
+		System.out.println(method+": ("+x+","+y+") = "+iCalculator.calculate(x, y));
+	}
 }
 public class MathApp {
 
@@ -23,6 +26,12 @@ public class MathApp {
 		System.out.println("Subtraction of 8 from 10 is "+subtract.calculate(10,8));
 		System.out.println("Divison of 9 by 0 is "+divide.calculate(9,0));
 		System.out.println("Divison of 12 by 3 is "+divide.calculate(12,3));
+		
+		ICalculator.printResult(6, 5, "Addition",add);
+		ICalculator.printResult(10, 8, "Subtraction", subtract);
+		ICalculator.printResult(9, 0, "Divison", divide);
+		ICalculator.printResult(12, 3, "Divison", divide);
+		
 	}
 
 }
